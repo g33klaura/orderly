a600d98a7b27d6aa9f2d86a208ec2866e96c9ffb
Laura Boetcher <g33klaura@gmail.com>
2018-01-20 00:57:54 -0500

    Test Materialize React rendering

--- resources/assets/js/components/Example.js
+++ resources/assets/js/components/Example.js
@@ -1,22 +1,31 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import { Container, Row, Col } from 'react-materialize';
 
 export default class Example extends Component {
     render() {
         return (
-            <div className="container">
-                <div className="row">
-                    <div className="col-md-8 col-md-offset-2">
-                        <div className="panel panel-default">
-                            <div className="panel-heading">Example Component</div>
+            // <div className="container">
+            //     <div className="row">
+            //         <div className="col-md-8 col-md-offset-2">
+            //             <div className="panel panel-default">
+            //                 <div className="panel-heading">Example Component</div>
 
-                            <div className="panel-body">
-                                I'm an example component!
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            //                 <div className="panel-body">
+            //                     I'm an example component!
+            //                 </div>
+            //             </div>
+            //         </div>
+            //     </div>
+            // </div>
+
+            <Container>
+                <Row>
+                    <Col s={12}>
+                        <h1>I'm in Materialize!</h1>
+                    </Col>
+                </Row>
+            </Container>
         );
     }
 }
