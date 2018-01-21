306248f04a22b9abd4f4193568ca51e1a64ac776
Laura Boetcher <g33klaura@gmail.com>
2018-01-20 19:54:47 -0500

    Declare fillable fields; add possible query scope for future

--- app/ProductBlank.php
+++ app/ProductBlank.php
@@ -8,4 +8,14 @@ class ProductBlank extends Model
 {
     // Declare associated table
     protected $table = 'product_blanks';
+
+    // Declare assignable attributes
+    protected $fillable = ['department', 'blank_type', 'blank_name', 'blank_color'];
+
+    // Define local scope function for repeatedly used query
+    public function scopeListTshirts( $query )
+    {
+        return $query->where('blank_type', 'T-shirt')
+            ->orderBy('blank_name', 'desc');
+    }
 }
