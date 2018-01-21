4fdb8017ca45521d3e2cafc97ccc7d391527c7c3
Laura Boetcher <g33klaura@gmail.com>
2018-01-20 21:37:47 -0500

    Assign relation and fillable attributes

--- app/ProductDetail.php
+++ app/ProductDetail.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProductDetail extends Model
 {
-    //
+    // Declare associated table
+    protected $table = 'product_details';
+
+    // Declare assignable attributes
+    protected $fillable = ['product_type', 'blank_name', 'blank_color', 'ink_colors', 'product_alias', 'pos_id', 'cost'];
+
+    // Assign relation to other tables
+    public function product()
+    {
+        return $this->belongsTo('App\Product', 'p_details_id');
+    }
+
 }
