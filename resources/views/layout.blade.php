93ad3d0903a112cf096cd68a22b2cf07437d20f1
Laura Boetcher <g33klaura@gmail.com>
2018-01-25 14:12:21 -0500

    Add Materialize links

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -6,7 +6,9 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <title>Welcome to Orderly</title>
-    <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:400,400i,600|Dosis" rel="stylesheet"> 
+    <link href="https://fonts.googleapis.com/css?family=Barlow+Semi+Condensed:400,400i,600|Dosis" rel="stylesheet">
+    <!-- Compiled and minified Materialize CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
   </head>
   <body>
 
@@ -15,5 +17,7 @@
 
     <!-- Script for React -->
     <script src="{{mix('js/app.js')}}"></script>
+    <!-- Compiled and minified Materialize JavaScript -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
   </body>
 </html>
