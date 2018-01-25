78592e16435a2d90b57916eb0e6e73c19570c5e3
Laura Boetcher <g33klaura@gmail.com>
2018-01-24 20:41:59 -0500

    Update model to use 'all_products' table

--- app/Product.php
+++ app/Product.php
@@ -7,14 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class Product extends Model
 {
     // Declare associated table
-    protected $table = 'products';
+    protected $table = 'all_products';
 
     // Declare assignable attributes
-    protected $fillable = ['product_name', 'department', 'graphic_size', 'ink_type', 'graphic_placement', 'image', 'product_notes', 'favorite_product'];
+    protected $fillable = ['product_name', 'department', 'graphic_size', 'graphic_placement', 'product_type', 'blank_name', 'blank_color', 'ink_colors', 'ink_type', 'pos_alias', 'cost', 'pos_id'];
 
     // Assign relation to other tables
-    public function productDetails()
-    {
-        return $this->hasMany('App\ProductDetail', 'id', 'p_details_id');
-    }
+    // public function productDetails()
+    // {
+    //     return $this->hasMany('App\ProductDetail', 'id', 'p_details_id');
+    // }
 }
