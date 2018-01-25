b83a8f14139beebb6b93c87e9e34f8917892e980
Laura Boetcher <g33klaura@gmail.com>
2018-01-24 20:41:37 -0500

    Add component for full product library; css for table scrollbar

---
+++ resources/assets/js/components/ProductsLibrary/Products.js
@@ -0,0 +1,91 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { Container, Row, Col, Table, Card } from 'react-materialize';
+import styles from './Products.css';
+
+class Products extends Component {
+  constructor() {
+    super();
+    this.state = {
+      products: [],
+    }
+  }
+
+  componentWillMount() {
+
+  }
+
+  componentDidMount() {
+    /* fetch API in action */
+    fetch('/api/products/')
+      .then(response => {
+        return response.json();
+      })
+      .then(products => {
+        //Fetched blanks is stored in the state
+        this.setState({ products });
+      });
+  }
+
+  componentWillReceiveProps(nextProps) {
+
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+
+  }
+
+  componentWillUpdate(nextProps, nextState) {
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+
+  }
+
+  componentWillUnmount() {
+
+  }
+
+  renderProducts() {
+    return this.state.products.map(product => {
+      return (
+        <tr key={product.id}>
+          <td>{product.product_name}</td>
+          <td>{product.blank_name}</td>
+        </tr>
+      );
+    })
+  }
+
+  render() {
+    return (
+        <Container>
+        <meta name="csrf-token" content="{{ csrf_token() }}"></meta>
+          <Row>
+            <Col s={12}>
+              <div className='material-table'>
+                <Table className='material-table'>
+                  <thead>
+                    <tr>
+                      <th>Name</th>
+                      <th>Blank</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                      { this.renderProducts() }
+                  </tbody>
+                </Table>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+    );
+  }
+}
+
+Products.propTypes = {
+
+};
+
+export default Products;
