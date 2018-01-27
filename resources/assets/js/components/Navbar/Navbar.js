724d4213c14d73be46b95f69c96bef008dfe8c34
Laura Boetcher <g33klaura@gmail.com>
2018-01-26 22:47:41 -0500

    Update to correct logo for navbar & add margins from sides of screen

--- resources/assets/js/components/Navbar/Navbar.js
+++ resources/assets/js/components/Navbar/Navbar.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Navbar, NavItem, SideNav, SideNavItem } from 'react-materialize';
-import  OrderlyLogo  from '../../../img/OrderlyLogo.svg';
+// import OrderlyLogo from '../../../img/OrderlyLogo.svg';
+import OrderlyLogobw from '../../../img/OrderlyLogobw.svg';
 import styles from './Navbar.css';
 
-const Logo = <img id='nav-logo' src={ OrderlyLogo }/>
+const Logo = <img id='nav-logo' src={ OrderlyLogobw }/>
 
 class Nav extends Component {
   render() {
