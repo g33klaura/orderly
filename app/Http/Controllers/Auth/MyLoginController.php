983640ecf5ee93d0fc598ce1875fec5b14ce31ab
Laura Boetcher <g33klaura@gmail.com>
2018-01-27 20:16:40 -0500

    Might set custom logout functions here for React Components

---
+++ app/Http/Controllers/Auth/MyLoginController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use Illuminate\Http\Request;
+
+// May set this to extends LoginController... (and still need to "use" it above)
+class MyLoginController extends Controller
+{
+    //
+}
